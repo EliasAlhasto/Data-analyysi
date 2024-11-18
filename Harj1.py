e893849d2a3db5ebc508e093e93a0a2413d338dc
Elias Alhasto <elias.alhasto@student.hamk.fi>
2024-11-18 09:04:21 +0200

    muutokset

---
+++ Harj1.py
@@ -0,0 +1,25 @@
+print("Printer App")
+
+print("This app is made by:", end=" ")
+
+creator = "Elias"
+
+print("Elias")
+num1 = 5
+num2 = 3
+
+print(f"The value of number 1 is {num1}")
+print(f"The value of number 2 is {num2}")
+
+multiplication = num1 * num2
+
+print(f"{num1} * {num2} = {multiplication}")
+
+sum = num1 + num2
+print(f"{num1} + {num2} = {sum}")
+
+subtraction = num1 - num2
+print(f"{num1} - {num2} = {subtraction}")
+
+division = num1 / num2
+print(f"{num1} / {num2} = {division: .2f}")
