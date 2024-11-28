f2c230f31c5480c74f76ac8dfa4a90fc252f36b9
Elias Alhasto <elias.alhasto@student.hamk.fi>
2024-11-28 21:25:23 +0200

    muutokset

--- Exercise-If-Excercises.py
+++ Exercise-If-Excercises.py
@@ -1,7 +1,7 @@
-number1 = 30
+number1 = 20
 number2 = 20
 
-if number1 > number2:
-    print("number1 is greater than number2")
+if number1 >= number2:
+    print("number1 is greater or equal to number2")
 else:
-    print("number1 is not greater than number2")
+    print("number1 is not greater or equal to number2")
