abed6753f7a8e36531e5b1646737f8519b97c473
Elias Alhasto <elias.alhasto@student.hamk.fi>
2024-11-28 21:33:25 +0200

    2a-e

--- Exercise-If-Excercises.py
+++ Exercise-If-Excercises.py
@@ -1,7 +1,30 @@
-number1 = 30
-number2 = 30
+number1 = 10
+number2 = 10
+number3 = 5
 
-if number1 != number2:
-    print("number1 is NOT equal to number2")
+#2a
+print("All numbers are equal:", number1 == number2 == number3)
+
+#2b
+print("Either number1 == number2 OR number2 == number3:", 
+      number1 == number2 or number2 == number3)
+
+#2c
+print("number1 > number2 AND number1 > number3:", 
+      number1 > number2 and number1 > number3)
+
+#2d
+if number1 > number2:
+    print("number1 > number2")
+elif number2 > number3:
+    print("number2 > number3")
 else:
-    print("number1 is equal to number2")
+    print("Neither condition is true")
+
+#2e
+if number1 == number2:
+    print("number1 == number2")
+elif number1 == number3:
+    print("number1 == number3")
+else:
+    print("None of them are the same")
