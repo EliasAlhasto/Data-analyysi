b1c50afcb80aa4440fb45dffbe6ddedbffb6a327
Elias Alhasto <elias.alhasto@student.hamk.fi>
2024-12-04 13:33:16 +0200

    highest value

--- List-Excercises.py
+++ List-Excercises.py
@@ -10,3 +10,6 @@ for _ in range(5):
 
     total_sum = sum(thrownDice)
     print("\nSum of the thrown dice numbers:", total_sum)
+
+    highest_value = max(thrownDice)
+print("\nHighest value is:", highest_value)
